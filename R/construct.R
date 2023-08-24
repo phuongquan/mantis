ec613fca4a0f9fb41f8584c2cf94fb2e32586761
Phuong Quan <phuong.Quan@ukhsa.gov.uk>
2023-08-24 16:54:32 +0100

    add new func construct_rmd_tab_item()

--- R/construct.R
+++ R/construct.R
@@ -1,4 +1,69 @@
 
+#' Dynamically generate a single tab for an rmd chunk
+#'
+#' Chunk options must contain `results = 'asis'`.
+#' Function writes directly to the chunk using side-effects
+#'
+#' @param tab_name
+#' @param df
+#' @param timepoint_col
+#' @param item_col
+#' @param value_col
+#' @param tab_col
+#' @param tab_order
+#' @param history_type
+#' @param timepoint_limits
+#' @param fill_with_zero
+#' @param item_order
+#' @param item_label
+#' @param summary_cols
+#' @param history_style
+#' @param sync_axis_range
+#'
+#' @return
+#' @export
+construct_rmd_tab_item <- function(df,
+                              timepoint_col,
+                              item_col,
+                              value_col,
+                              tab_name = NULL,
+                              history_type = "value",
+                              timepoint_limits = c(NA, NA),
+                              fill_with_zero = FALSE,
+                              item_order = NULL,
+                              item_label = "Item",
+                              summary_cols = c("last_value", "mean_last14"),
+                              history_style = "bar",
+                              sync_axis_range = FALSE) {
+
+  # TODO: consider allowing tab level to be passed in as a param
+  if (!is.null(tab_name)) {
+    cat("\n###", tab_name, "\n")
+  }
+
+  p <-
+    prepare_table(
+      df,
+      timepoint_col,
+      item_col,
+      value_col,
+      history_type = history_type,
+      timepoint_limits = timepoint_limits,
+      fill_with_zero = fill_with_zero,
+      item_order = item_order
+    ) %>%
+    output_table_html(
+      item_label = item_label,
+      summary_cols = summary_cols,
+      history_style = history_style,
+      sync_axis_range = sync_axis_range
+    )
+  # NOTE: a regular print() doesn't render the widget
+  cat(knitr::knit_print(p))
+  cat("\n")
+
+}
+
 #' Dynamically generate a group of tabs for an rmd chunk
 #'
 #' Chunk options must contain `results = 'asis'`.
@@ -52,28 +117,46 @@ construct_rmd_tab_group <- function(df,
   }
 
   for (i in seq_along(tab_names)) {
+    dftab <-
+      df %>% dplyr::filter(.data[[tab_col]] == tab_names[i])
 
-    cat("\n###", tab_names[i], "\n")
-    p <-
-      prepare_table(
-        df %>% dplyr::filter(.data[[tab_col]] == tab_names[i]),
-        timepoint_col,
-        item_col,
-        value_col,
-        history_type = history_type,
-        timepoint_limits = timepoint_limits,
-        fill_with_zero = fill_with_zero,
-        item_order = item_order
-        ) %>%
-        output_table_html(
-          item_label = item_label,
-          summary_cols = summary_cols,
-          history_style = history_style,
-          sync_axis_range = sync_axis_range
-        )
-      # NOTE: a regular print() doesn't render the widget
-      cat(knitr::knit_print(p))
-      cat("\n")
+    construct_rmd_tab_item(
+      df = dftab,
+      timepoint_col = timepoint_col,
+      item_col = item_col,
+      value_col = value_col,
+      tab_name = tab_names[i],
+      history_type = history_type,
+      timepoint_limits = timepoint_limits,
+      fill_with_zero = fill_with_zero,
+      item_order = item_order,
+      item_label = item_label,
+      summary_cols = summary_cols,
+      history_style = history_style,
+      sync_axis_range = sync_axis_range
+    )
+
+    # cat("\n###", tab_names[i], "\n")
+    # p <-
+    #   prepare_table(
+    #     df %>% dplyr::filter(.data[[tab_col]] == tab_names[i]),
+    #     timepoint_col,
+    #     item_col,
+    #     value_col,
+    #     history_type = history_type,
+    #     timepoint_limits = timepoint_limits,
+    #     fill_with_zero = fill_with_zero,
+    #     item_order = item_order
+    #     ) %>%
+    #     output_table_html(
+    #       item_label = item_label,
+    #       summary_cols = summary_cols,
+    #       history_style = history_style,
+    #       sync_axis_range = sync_axis_range
+    #     )
+    #   # NOTE: a regular print() doesn't render the widget
+    #   cat(knitr::knit_print(p))
+    #   cat("\n")
   }
 
 }
